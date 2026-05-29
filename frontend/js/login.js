7bb89f9527080fe676d100d6a35934d5233d455c
Thiago Mendes <thiagomendesnetwork@gmail.com>
2026-05-29 06:40:44 -0300

    corrigindo login.js

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -1,3 +1,41 @@
+const usuariosPadrao = [
+
+  {
+    nome: 'Administrador',
+    email: 'admin',
+    senha: '123',
+    cargo: 'Administrador'
+  },
+
+  {
+    nome: 'Caixa',
+    email: 'caixa',
+    senha: '123',
+    cargo: 'Caixa'
+  },
+
+  {
+    nome: 'Garcom',
+    email: 'garcom',
+    senha: '123',
+    cargo: 'Garçom'
+  }
+
+];
+
+if(!localStorage.getItem('usuarios')) {
+
+  localStorage.setItem(
+    'usuarios',
+    JSON.stringify(usuariosPadrao)
+  );
+
+}
+
+
+
+
+
 function entrar() {
 
   const usuarios = JSON.parse(
